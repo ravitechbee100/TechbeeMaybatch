b9e66b8d60442d3f767949ef8cf3e3fe26f7d3c8
IzunduNasa <72169303+IzunduNasa@users.noreply.github.com>
2022-05-19 13:58:57 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,50 +1,73 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-class Main <T>{
+public class Main {
     public static void main(String[] args) {
-        Helpers helpers = new Helpers();
 
-        //Test Java Program to find average of an int Array.
-        var intarr = new int[]{3,4,2,6,8,1,9,5};
-        System.out.println("Average of "+ Arrays.toString(intarr) + " is " + helpers.Average(intarr));
+        System.out.println("Hello world!");
 
-        //Test Create a Java Program to Sort an Array of Numeric or String Data.
-        var objarri = new Object[]{3,4,2,6,8,1,9,5};
-        var objarrs = new Object[]{"a", "d", "c", "egg", "ball", "f", "apple"};
-        helpers.Sort(objarri);
-        helpers.Sort(objarrs);
-        System.out.println("Sort int: " +Arrays.toString(objarri));
-        System.out.println("Sort str: " +Arrays.toString(objarrs));
-
-        //Test Write a Java Program to check if Array contains a specific value.
-        System.out.println("Find '4': " +helpers.Find(objarri, 4));
-
-        //Test Write a Java Program to reverse the array contents.
-        helpers.Reverse(objarrs);
-        System.out.println("Reverse: " +Arrays.toString(objarrs));
-
-        //Test Write a Java Program to remove a particular element from an array.
-        helpers.Remove(objarri, 4);
-        System.out.println("Remove '4': " +Arrays.toString(objarri));
-
-        //Test Write a Java Program to find the second highest value in a numeric array.
-        System.out.println("Second highest: " +helpers.SecondHighest(intarr));
-
-        //Test Create a Java code to copy an Array into another array.
-        var arr1 = new Object []{1,2,3,4};
-        var arr2 = new Object []{5,6,7};
-        var arr = helpers.Copy(arr1, arr2);
-        System.out.println("Copy: " + Arrays.toString(arr1) + "+" + Arrays.toString(arr2) + "="+Arrays.toString(arr));
-
-        //Test Write a Java Code a Array of length 100 and fill it with Random int Values for testing purpose.
-        System.out.println("Random 100: " +Arrays.toString(helpers.Random100()));
-
-        //Test Java program to find duplicate elements in a String array.
-        var strarr = new String[]{"a", "d", "c", "egg", "ball", "f", "apple", "a", "egg"};
-        System.out.println("String Duplicates: For array" +Arrays.toString(strarr));
-        helpers.DuplicateChars(strarr);
-
-        //Test Write a Java program to check if two arrays are equal.
-        System.out.println("Equal: "  + Arrays.toString(arr1) + "and" + Arrays.toString(arr2) + "="+helpers.EqualArrays(arr1, arr2));
     }
+
+    static void PrintArray(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of items: ");
+        int len = scan.nextInt();
+        int [] arr = new int[len];
+        System.out.println("Enter all items: ");
+        for(int i=0;i<len;i++){//for reading array
+            arr[i]=scan.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+        scan.close();
+    }
+
+    static void ReverseString(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = scan.nextLine();
+        System.out.println("Reverse is: ");
+
+        for(int i=str.length();i>=0;i--){//for reading array
+            System.out.print(str.charAt(i));
+        }
+
+        scan.close();
+    }
+
+    static void CountVowelsDigits(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = scan.nextLine();
+        int vow = 0;
+        double percent = vow/str.length() * 100;
+
+
+        for(int i=str.length();i>=0;i--){//for reading array
+            String c = String.valueOf(str.charAt(i));
+            if(c.toLowerCase().equals("a") ||c.toLowerCase().equals("e") ||c.toLowerCase().equals("i")||c.toLowerCase().equals("o")||c.toLowerCase().equals("u") ){
+                vow++;
+            }
+        }
+        System.out.println("Num of vowels is "+ vow +percent);
+        System.out.println("Num of digits is "+ str.length() + (100 - percent));
+        scan.close();
+    }
+
+    static void TestPalindromicWord(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = scan.nextLine();
+
+        StringBuffer sbr = new StringBuffer(str);
+
+        sbr.reverse();
+        if(sbr.equals(str)){
+            System.out.println(str + "is a palindrome");
+        }
+        else System.out.println(str + "is not a palindrome");
+
+        scan.close();
+    }
+
 }
