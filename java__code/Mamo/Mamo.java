d6104e8fabd0c13eb6d0e39355273e241427a5ae
Mengistayehu Mamo <mengistayehu@yahoo.com>
2022-05-22 11:03:50 -0700

    CountVowelsDigits,a palindrome missing from yesterday push

--- java__code/Mamo/Mamo.java
+++ java__code/Mamo/Mamo.java
@@ -1,15 +1,82 @@
 package Mamo;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Mamo {
 	
+	public static void TestPalindromicWord() {
+		
+		System.out.println("Enter Any String");
+		Scanner s = new Scanner(System.in);
+		
+		String origin = s.nextLine();
+		int length = origin.length();
+		
+		boolean isPalindrome = true;
+		
+		for(int startIndex = 0; startIndex < length; startIndex++) 
+		{
+			if(origin.charAt(startIndex) != origin.charAt(length- 1 - startIndex)) {
+				
+				System.out.print("String is not palindrome");
+				isPalindrome = false;
+				break;
+			}
+			
+		}
+		if(isPalindrome) {
+			System.out.print("String is palindrome");
+
+		}
+		
+		s.close();
+		
+		
+		
+	}
+	
+	
+	public static int[] removeElement(int[] arrayRemovable, int index) {
+		
+		 if(arrayRemovable == null || index < 0  || index >= arrayRemovable.length ) {
+			 
+	    	 return arrayRemovable;
+	     }
+		 
+	        // Create ArrayList from the array
+		 
+		 List<Integer> arrayList = IntStream.of(arrayRemovable).boxed().collect(Collectors.toList());
+		 
+		 arrayList.remove(index)
+;
+//		 int [] anotherArray = new int[arrayRemovable.length -1];
+//		
+//		for(int i = 0, k = 0; arrayRemovable.length; i++) {
+//			
+//			if(i == index) {    
+//				continue;
+//			}
+//			anotherArray[k++] = arrayRemovable[i];
+//		}
+		 
+		return arrayList.stream().mapToInt(Integer:: intValue).toArray();
+	}
 	
 	
 	public static void main(String[] args) {
 		
 		
 		//Java Program to find average of an int Array.  
+		
+		    TestPalindromicWord();
+		    
+		    System.out.println("***************");
 		
 		
 		   int [] array = {23, 34, 65, 76, 89, 76, 54, 23};
@@ -83,15 +150,29 @@ public class Mamo {
 	     }
 	     //Write a Java Program to remove a particular element from an array. 
 	     
-	     int [] arrayRemovable; int  index;
 	     
-	     //if()
+	     // Get the array
+	        int[] arr = { 1, 2, 3, 4, 5 };
+	 
+	        // Print the resultant array
+	        System.out.println("Original Array: "
+	                           + Arrays.toString(arr));
+	 
+	        // Get the specific index
+	        int index = 2;
+	 
+	        // Print the index
+	        System.out.println("Index to be removed: " + index);
+	 
+	        // Remove the element
+	        arr = removeElement(arr, index);
+	 
+	        // Print the resultant array
+	        System.out.println("Resultant Array: "
+	                           + Arrays.toString(arr));
+	    
 	     
-	     
-	     
-	     
-	     
-	     
+  
 	     
 //	     2. Write a program called CountVowelsDigits, which prompts the user for a String, counts the number of
 //	     vowels (a, e, i, o, u, A, E, I, O, U) and digits (0-9) contained in the string, and prints the counts and the
@@ -130,7 +211,10 @@ public class Mamo {
 	    System.out.println("consonant count: " + consonant);
 	    System.out.println("special Charactor count: " +speecialChar);
 
-
+       //called TestPalindromicWord,
+	    
+	    
+	    
 	}
 
 }
